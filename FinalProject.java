1ef064a3b91ff01f5c284dcb5f7dc2c0b5f235e8
zhenweigc <huruiyangsam@gmail.com>
2017-11-23 21:06:31 -0500

    Add files via upload

---
+++ FinalProject.java
@@ -0,0 +1,31 @@
+//This is the final project program
+public class FinalProject{
+  //This string array contain map information
+  public static final String[][] Map=
+  {
+    {"Mw","Fn","Sn","Rr","Rr","Hw","Wt","Fr","Hr","Mr","Gr"},
+    {"Mr","Mr","Dr","Tw","Rr","Fn","Tw","Rr","Fn","Aw","Wn"},
+    {"Fn","Fn","Wr","Pr","Hr","Dr","Tw","Sw","Mr","Aw","Hw"},
+    {"Tr","Hr","Fn","Fn","Fw","Rr","Fw","Or","Or","Dw","Ow"},
+    {"Hn","Ar","Rr","Ar","Wt","Rr","Ww","Hr","Dn","Wn","Ow"},
+    {"Wb","Ar","Rr","Wr","Ob","Pr","Wn","Fr","Pr","Wb","Mr"},
+    {"An","Rr","Rr","Ow","Aw","On","Wt","Mr","Hr","Mr","Mr"},
+    {"An","Rr","Rt","Sn","An","An","Dw","Ar","Tw","Pr","An"},
+    {"Mr","On","On","Ow","An","On","Tw","Sn","Wr","Aw","An"},
+    {"Hr","Sw","Wt","On","On","On","Tb","Aw","Dn","Fn","Sn"},
+    {"Fn","Ar","Ar","Dn","Dr","An","Tn","Pr","Hr","Or","Wn"},
+    {"An","Pr","Aw","Dw","Hr","At","Tn","Ar","Fw","Or","Pr"},
+    {"An","An","At","Ar","Mr","Ft","On","Ar","Fw","Fw","Fb"},
+    {"Ar","Ar","Sn","Ar","Rt","Ww","Wn","Ww","Fb","Pr","Fn"},
+    {"Aw","Aw","Tn","Wt","Rr","Pr","Hr","Ww","Fn","Fn","Fw"}
+  };
+  public static void main(String[] args){
+    /**  for(int i=0;i<15;i++){
+        for(int j=0;j<11;j++){
+          System.out.print(Map[i][j]+" ");
+        }
+        System.out.println();
+      }
+*/
+    }
+  }
